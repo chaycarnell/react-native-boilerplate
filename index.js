7f79f88861c89dbd3d2dfa3e87b70eb840016cb0
Chay Carnell <chaycarnell@gmail.com>
2024-10-21 22:38:13 +1000

    build(rn upgrade): upgrade react native version 0.75.4

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-/**
- * @format
- */
+import './src/translations';
 
 import { AppRegistry } from 'react-native';
+
 import { displayName as appName } from './app.json';
 import App from './src/App';
-import './src/translations';
 
 AppRegistry.registerComponent(appName, () => App);
